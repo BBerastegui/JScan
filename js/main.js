2c34d43d98553bd7f4bc59a2d8a968dfceacdc76
gea <gea@gea.gea.local>
2015-01-18 18:26:00 +0100

    WIP. REALLY. This is embarrasing at least for now.

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// CLICKS
+$('#parse_btn').click( function() {
+	parseUrl(document.getElementById("parse_url").value);
+});
+
+function parseUrl(url){
+	doRequest(url,'GET','','');
+	//	alert(url);
+}
+
+function doRequest(url, method, header, body){
+	$.ajax({
+		url: "http://localhost:8080",
+		context: document.body,
+		type: "POST",
+		data: '{"Method":"'+method+'","URL":"'+url+'","Header":{'+header+'},"body":"'+body+'"}'
+	}).done(function(response) {
+		console.log("Sending response to be printed...");
+		printResultsParse(response);
+	});
+}
+
+function printResultsParse(json){
+	var objJson = JSON.parse(json);
+
+	var table = document.getElementById("parse_results");
+	var header = table.createTHead();
+	var headerRow = header.insertRow(0);
+/*	
+	for (i = 0; i < Object.keys(objJson).length; i++){
+		var headerCell = headerRow.insertCell(i);
+		headerCell.innerHTML = Object.keys(objJson)[i];
+	}
+*/
+
+	elements = parseHTML(objJson.Body, "a");
+	
+	for (i = 0; i < elements.length; i++){
+		var row = table.insertRow(i);
+		var cell = row.insertCell(0);
+		cell.innerHTML = elements[i].innerHTML;
+		var cell = row.insertCell(0);
+		cell.innerHTML = elements[i].href;
+	}
+}
+
+function parseHTML(html, tag){
+	elements = $(html).find(tag);
+	console.log(elements);
+	// If tag a, fix the hrefs to get full references
+	if (tag == "a"){
+	for (i = 0; i < elements.length; i++){
+		if (elements[i].host == ""){
+			console.log(elements[i]);
+			// TODO Fix the uncompleted href !!!
+			elements[i].href = "[HOST]"+elements[i].attributes.href.value;
+			console.log(elements[i].href);
+		}
+	}
+	}
+	return elements;
+}
